2736b4e64b344ee7fb64d853f2c4ce62f7100e3a
EMDOURADO <eliel.dourado27@gmail.com>
2016-04-06 22:32:46 -0300

    2.0!

---
+++ AgendaTelefonica/src/ch/agenda/main/controler.java
@@ -0,0 +1,42 @@
+package ch.agenda.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+	public class controler {
+
+	    @FXML
+	    private Button IdExcluir;
+
+	    @FXML
+	    private Button IdSalvar;
+
+	    @FXML
+	    private Button IdLimpar;
+
+	    @FXML
+	    private TextField IdUsuario;
+
+	    @FXML
+	    private TextField IdNome;
+
+	    @FXML
+	    private TextField IdTelefone;
+
+	    @FXML
+	    private TableView<?> Id;
+
+	    @FXML
+	    private TableColumn<?, ?> IdC;
+
+	    @FXML
+	    private TableColumn<?, ?> IdCNome;
+
+	    @FXML
+	    private TableColumn<?, ?> IdCTelefone;
+
+	}
+
